a3dfee576ead8c52cd15474df9e21a8d8bc8c26a
Rajat Pandey <rajatpndey257@gmail.com>
2025-06-12 14:26:33 +0530

    Created Root layout and constants

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// export const metadata = {
+//   title: 'Home'
+// }
+const Homepage = () => {
+  return (
+    <div className='text-xl'>
+      Prostore
+    </div>
+  )
+}
+
+export default Homepage
