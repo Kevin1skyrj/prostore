2b608fcf23da69a85638720b61a8fa4bff6f0d85
Rajat Pandey <rajatpndey257@gmail.com>
2025-06-13 15:58:39 +0530

    Created theme toggle button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -18,13 +20,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
        <head>
         <link rel="icon" href="/favicon.ico" />
         {/* You can add more icons or meta tags here */}
       </head>
       <body className={`${inter.className} antialiased`} suppressHydrationWarning={true}>
-        {children}
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+        >
+           {children}
+        </ThemeProvider>
+       
       </body>
     </html>
   )
