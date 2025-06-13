1258d556430eb913a82913c6c0d40ef8cf479033
Rajat Pandey <rajatpndey257@gmail.com>
2025-06-13 20:12:37 +0530

    Created loading page and not found page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+// const delay=(ms) => new Promise((resolve) => setTimeout(resolve, ms));
 // export const metadata = {
 //   title: 'Home'
 // }
 const Homepage = () => {
+  
   return (
     <div className='text-xl'>
       Prostore
